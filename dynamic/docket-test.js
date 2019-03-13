d5b915ddfdd384c2bae5e55ed2eebfa1e51705ea
U-LTPB\U0149113 <U0149113@u0149113-tpd-c.mm-ads.com>
2019-03-13 08:34:57 -0400

    tt1

--- dynamic/docket-test.js
+++ dynamic/docket-test.js
@@ -7,12 +7,14 @@ const nightmare = Nightmare({ show: true });
 require('nightmare-iframe-manager')(Nightmare);
 const url = 'https://thelens.clarivate.com/groups/technology-asset-management/projects/ipdocketingportal/content';
 const { promisify } = require('util')
-//const writeFile = promisify(fs.writeFile)
+var _ = require('lodash');
+var fs = require('fs');
+const writeFile = promisify(fs.writeFile)
 //const readFile = promisify(fs.readFile)
 require('nightmare-inline-download')(Nightmare);
 require('nightmare-download-manager')(Nightmare);
 
-const cookie =  {"Cookie": "_sp_id.2f26=2f13d6d5-1037-49ac-a983-3d6bbfaf6823.1540307720.4.1542735343.1542385389.a01ba71e-eb78-4cfa-91cd-898f7ab96a1d; sp=3e1d0f0b-d35c-4fc1-a853-7f610099e9c9; _gid=GA1.2.511184856.1552253364; _ga=GA1.2.683619370.1511194813; _gat=1; route=7386de0ddbbd7aca06bbe78266438619; jive.login.ts=1552411801030; JSESSIONID=A23DE30CFEA75D13E9254E3D061F6860; jive.security.context=gcNnEEw9zj/rfL9j2P7hyAAAAAAADs+fqw9rXZAGN8nNN2pMO6sgPSbSvGVk37ufCuoZDpC7/bfTD1bRfvvh0Sf1jPY=; jive.user.loggedIn=true; jive.server.info=serverName=thelens.clarivate.com:serverPort=443:contextPath=:localName=webapp-rc-fsxr5:localPort=9443:localAddr=172.16.130.21; X-JCAPI-Token=zNqQ3lG5; jive.loginEventFired=true; containerSecurityToken=default:edUIEU-S0D2aIh6HjIjlE7IUGb_RCEYz2KYbWGLUuVuQfBnssb8PfgH7ZmAgbadwnwH-c47xpL820fN7nqWosewpcyLaZBHk8IOX6Q65A8ezOrZ8#3600#false#1552418896353; _csrf=90PVT1OzSR3-havZ_3YCH1qH; jive.login.type=saml"};
+const cookie =  {"Cookie": "_sp_id.2f26=2f13d6d5-1037-49ac-a983-3d6bbfaf6823.1540307720.4.1542735343.1542385389.a01ba71e-eb78-4cfa-91cd-898f7ab96a1d; sp=3e1d0f0b-d35c-4fc1-a853-7f610099e9c9; _gid=GA1.2.511184856.1552253364; _ga=GA1.2.683619370.1511194813; _gat=1; jive.login.type=saml; route=7386de0ddbbd7aca06bbe78266438619; jive.login.ts=1552470971341; JSESSIONID=F0D06FAE1EE18A15F728C6AD60243AE7; jive.security.context=/k6wcln5ZE+2TQQFgAdiKAAAAAAADtVpmWMf8ZO/iDzQsPOX+2hiO+MXp2/ZW1vfBvZDN7RSQDpn8OsbeqUOhJr1Rh0=; jive.user.loggedIn=true; jive.server.info=serverName=thelens.clarivate.com:serverPort=443:contextPath=:localName=webapp-rc-fsxr5:localPort=9443:localAddr=172.16.130.21; X-JCAPI-Token=5un8V7FA"};
 
 // const writeAndRead = async () => {
 //     await writeFile('./test.txt', 'Hello World')
@@ -55,8 +57,8 @@ const download = ( async (url) => {
         });
 })
 
-function doRequest(data) {
-    return new Promise(function (resolve, reject) {
+ async function doRequest(data) {
+    return new Promise( async function (resolve, reject) {
         request({
             method: 'GET',
             headers:  cookie,
@@ -70,11 +72,17 @@ function doRequest(data) {
 
             var  files= [];
             var att =  $('a.j-attachment-icon').each(function(i, elem){
-                files[i] = $(this).attr("href");               
-            })  
+                files[i] = 'https://thelens.clarivate.com/'.concat($(this).attr("href"));
+            })
+            _.each(files, download1);
+            var  images = [];
+            var att =  $('div.jive-rendered-content img').each(function(i, elem){
+                images[i] = $(elem).attr("src");
+            })
+             _.each(images, download1);
             //console.log(text);
             resolve( {
-                "title": title, "text": text , "filess" : files
+                "title": title, "text": text , "filess" : files, "images" : images
             });
         } else {
           reject(error);
@@ -97,6 +105,23 @@ function dobaseRequest() {
         console.log(data);
         resolve(data);
         });
+    })
+}
+
+async function download1(url, i) {
+    return new Promise( async function (resolve, reject) {
+        var filename, filepath, file;
+        filename = url.split('/');
+        filename = filename[filename.length - 1];
+        filepath = [__dirname, '/downloads/', filename].join('');
+
+        function callback() {
+                  console.log(filename, 'done');
+                resolve('done')
+        }
+
+        file = fs.createWriteStream(filepath);
+       await request(url).pipe(file).on('close',  callback);
     })
 }
 
@@ -146,8 +171,25 @@ function dobaseRequest() {
         //insertPage(res.title, res.text);
     }
 
-    // let res = await doRequest("/docs/DOC-3493728");
-     console.log(data1);
+      var filename, filepath, file;
+
+      filename = 'page1.json';
+      filepath = [__dirname, '/downloads/', filename].join('');
+      function callback() {
+
+          console.log(filename, 'done');
+
+      }
+
+
+
+      file = fs.createWriteStream(filepath);
+
+      await writeFile(filepath, JSON.stringify(data1));
+      //data1.pipe(file).on('close', callback);
+
+      // let res = await doRequest("/docs/DOC-3493728");
+     //console.log(data1);
 }
   
   main();
